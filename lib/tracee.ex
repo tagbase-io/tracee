0865779bf68b46b5cd43d98a4a6fe6b89853697b
Mario Uher <uher.mario@gmail.com>
2024-04-21 19:33:26 +0200

    Fix race condition

--- lib/tracee.ex
+++ lib/tracee.ex
@@ -1,10 +1,15 @@
 defmodule Tracee do
-  @moduledoc false
+  @moduledoc """
+  Provides functionality to trace and assert expected function calls within Elixir processes.
+  """
 
   import ExUnit.Assertions, only: [assert: 1]
 
   @name {:global, __MODULE__}
 
+  @doc """
+  Starts the tracer.
+  """
   def start_link do
     case Agent.start_link(fn -> %{} end, name: @name) do
       {:error, {:already_started, _}} ->
@@ -20,7 +25,10 @@ defmodule Tracee do
                pid
                |> :erlang.process_info()
                |> get_in([:dictionary, :"$ancestors"])
-               |> List.last()
+               |> case do
+                 list when is_list(list) -> List.last(list)
+                 _ -> pid
+               end
 
              Agent.update(@name, fn state ->
                state
@@ -33,6 +41,8 @@ defmodule Tracee do
                  count -> count + 1
                end)
              end)
+
+             send(test, {Tracee, :agent_updated})
            end, nil}
         )
 
@@ -42,6 +52,9 @@ defmodule Tracee do
     end
   end
 
+  @doc """
+  Sets an expectation for a function call with a specific arity and optional count.
+  """
   def expect(module, function, arity, count \\ 1) do
     test = self()
 
@@ -59,15 +72,33 @@ defmodule Tracee do
     :ok
   end
 
-  def verify_on_exit! do
+  @doc """
+  Registers a verification check to be performed when the current test process exits.
+  """
+  def verify_on_exit!(_context \\ %{}) do
     test = self()
-    ExUnit.Callbacks.on_exit(Tracer, fn -> verify(test) end)
+
+    ExUnit.Callbacks.on_exit(Tracer, fn ->
+      verify(test)
+      :dbg.stop()
+    end)
   end
 
-  defp verify(pid) do
-    case Agent.get(@name, &Map.get(&1, pid)) do
-      %{expectations: expectations, traces: traces} when map_size(expectations) > 0 -> assert expectations == traces
-      _ -> nil
+  @doc false
+  def verify(pid) do
+    # Wait for the agent to be update to date.
+    receive do
+      {Tracee, :agent_updated} -> nil
+    after
+      10 -> nil
+    end
+
+    case Agent.get(@name, & &1) do
+      %{^pid => %{expectations: expectations, traces: traces}} when map_size(expectations) > 0 ->
+        assert expectations == traces
+
+      _ ->
+        nil
     end
   end
 end
--- test/tracee_test.exs
+++ test/tracee_test.exs
@@ -1,7 +1,20 @@
 defmodule TraceeTest do
   use ExUnit.Case
 
-  test "start_link/0" do
-    assert Tracee.start_link() == :ignore
+  defmodule TestModule do
+    @moduledoc false
+    def arity_0, do: :ok
+    def arity_1(_arg), do: :ok
+  end
+
+  test "expect/4 ensures function is called" do
+    Tracee.expect(TestModule, :arity_0, 0)
+    Tracee.expect(TestModule, :arity_1, 1, 2)
+
+    assert TestModule.arity_0() == :ok
+    assert TestModule.arity_1(:ok) == :ok
+    assert TestModule.arity_1(:ok) == :ok
+
+    Tracee.verify(self())
   end
 end
